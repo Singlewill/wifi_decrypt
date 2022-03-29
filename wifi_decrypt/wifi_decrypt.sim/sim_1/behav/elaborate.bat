f4c865ae57bac2b2f886cd4281f7ea838aa6abf7
Singlewill <156103665@qq.com>
2022-03-29 23:04:50 +0800

    删除了工程文件

--- wifi_decrypt/wifi_decrypt.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.1\\bin
-call %xv_path%/xelab  -wto 5600bfe3825145c0b9a191f8bb43f8cc -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip -L xpm --snapshot TB_WIFI_DECRYPT_behav xil_defaultlib.TB_WIFI_DECRYPT -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- wifi_decrypt/wifi_decrypt.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.1\\bin
-call %xv_path%/xsim TB_WIFI_DECRYPT_behav -key {Behavioral:sim_1:Functional:TB_WIFI_DECRYPT} -tclbatch TB_WIFI_DECRYPT.tcl -view D:/workdir/vivado_project/wifi_decrypt/wifi_decrypt/TB_WIFI_DECRYPT_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
